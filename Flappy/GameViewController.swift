f75d848432aa610a8bc83642ace0063e3047cfa3
Drew Olbrich <drew.olbrich@gmail.com>
2017-05-21 19:09:25 -0700

    Add Flappy to scene.

--- Flappy/GameViewController.swift
+++ Flappy/GameViewController.swift
@@ -27,21 +27,17 @@ class GameViewController: UIViewController {
             
             view.ignoresSiblingOrder = true
             
-            view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsFPS = false
+            view.showsNodeCount = false
         }
     }
 
     override var shouldAutorotate: Bool {
-        return true
+        return false
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return .portrait
     }
 
     override func didReceiveMemoryWarning() {
