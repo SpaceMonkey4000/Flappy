4376012948c9084dbfdcee860bef9d1f6e52e29b
Drew Olbrich <drew.olbrich@gmail.com>
2017-05-22 19:34:16 -0700

    Add 8 flappy birds.

--- Flappy/GameScene.swift
+++ Flappy/GameScene.swift
@@ -1,14 +1,23 @@
 
 import SpriteKit
 
-class GameScene: Scene {
+class GameScene: SKScene {
 
-    var flappyNode = SKSpriteNode(imageNamed: "Flappy-5x")
-
-    override func start() {
-        addChild(flappyNode)
-
+    override func didMove(to view: SKView) {
         backgroundColor = UIColor.init(red: 0.0, green: 1.0, blue: 1.0, alpha: 1.0)
+
+        let birds = 8
+
+        for index in 0 ..< birds {
+            let angle: CGFloat = 2.0*CGFloat.pi*CGFloat(index)/CGFloat(birds)
+            let x: CGFloat = cos(angle)*200.0
+            let y: CGFloat = sin(angle)*200.0
+
+            let bird = SKSpriteNode(imageNamed: "Flappy-5x")
+            bird.position = CGPoint(x: x, y: y)
+            bird.zRotation = angle - CGFloat.pi/2.0
+            addChild(bird)
+        }
     }
 
 }
