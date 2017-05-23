80b5938ad4f532aebb0dd5932e896680eafb23fe
Drew Olbrich <drew.olbrich@gmail.com>
2017-05-22 19:25:53 -0700

    Add Scene.

--- Flappy/GameScene.swift
+++ Flappy/GameScene.swift
@@ -1,19 +1,14 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: Scene {
 
     var flappyNode = SKSpriteNode(imageNamed: "Flappy-5x")
 
-    required init?(coder decoder: NSCoder) {
-        super.init(coder: decoder)
-
+    override func start() {
         addChild(flappyNode)
-    }
 
-    override func didMove(to view: SKView) {
-        // The background color must be set in didMove(to:) instead of init(coder:), because reasons.
-        self.backgroundColor = UIColor.init(red: 0.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        backgroundColor = UIColor.init(red: 0.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
 
 }
