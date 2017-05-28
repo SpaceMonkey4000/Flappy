733ca45bf48e1d678dce2d121557bec220034eb6
Axel <axel@puppy.local>
2017-05-28 13:42:31 -0700

    even more bullets for some reason

--- Flappy/GameScene.swift
+++ Flappy/GameScene.swift
@@ -53,7 +53,10 @@ class GameScene: SKScene {
         let shoots3 = SKAction.moveBy(x:900, y: -100, duration: 1.25)
         let shoots4 = SKAction.moveBy(x:900, y: 200, duration: 1.5)
         let shoots5 = SKAction.moveBy(x:900, y: -200, duration: 1.5)
-        
+        let shoots6 = SKAction.moveBy(x:900, y: -300, duration: 1.75)
+        let shoots7 = SKAction.moveBy(x:900, y: 300, duration: 1.75)
+        let shoots8 = SKAction.moveBy(x:900, y: 400, duration: 2)
+        let shoots9 = SKAction.moveBy(x:900, y: -400, duration: 2)
         
         
         
@@ -100,37 +103,40 @@ class GameScene: SKScene {
             let bullet3 = SKSpriteNode (imageNamed: "bullet")
             let bullet4 = SKSpriteNode (imageNamed: "bullet")
             let bullet5 = SKSpriteNode (imageNamed: "bullet")
+            let bullet6 = SKSpriteNode (imageNamed: "bullet")
+            let bullet7 = SKSpriteNode (imageNamed: "bullet")
+            let bullet8 = SKSpriteNode (imageNamed: "bullet")
+            let bullet9 = SKSpriteNode (imageNamed: "bullet")
             bullet.zPosition = bird.zPosition - 1.0
             bullet2.zPosition = bird.zPosition - 1.0
             bullet3.zPosition = bird.zPosition - 1.0
             bullet4.zPosition = bird.zPosition - 1.0
             bullet5.zPosition = bird.zPosition - 1.0
+            bullet6.zPosition = bird.zPosition - 1.0
+            bullet7.zPosition = bird.zPosition - 1.0
+            bullet8.zPosition = bird.zPosition - 1.0
+            bullet9.zPosition = bird.zPosition - 1.0
             let movetobird = SKAction.move(to: bird.position, duration: 0)
             let shoot2 = SKAction.sequence([movetobird,shoots])
             let shoot3 = SKAction.sequence([movetobird,shoots2])
             let shoot4 = SKAction.sequence([movetobird,shoots3])
             let shoot5 = SKAction.sequence([movetobird,shoots4])
             let shoot6 = SKAction.sequence([movetobird,shoots5])
-            addChild(bullet)
-            bullet.run(shoot2) {
-                bullet.removeFromParent()
-            }
-            addChild(bullet2)
-            bullet2.run(shoot3){
-                bullet.removeFromParent()
-            }
-            addChild(bullet3)
-            bullet3.run(shoot4){
-                bullet.removeFromParent()
-            }
-            addChild(bullet4)
-            bullet4.run(shoot5){
-                bullet.removeFromParent()
-            }
-            addChild(bullet5)
-            bullet5.run(shoot6){
-                bullet.removeFromParent()
-            }
+            let shoot7 = SKAction.sequence([movetobird,shoots6])
+            let shoot8 = SKAction.sequence([movetobird,shoots7])
+            let shoot9 = SKAction.sequence([movetobird,shoots8])
+            let shoot10 = SKAction.sequence([movetobird,shoots9])
+            
+            
+            shootBullet(node: bullet, action: shoot2)
+            shootBullet(node: bullet2, action: shoot3)
+            shootBullet(node: bullet3, action: shoot4)
+            shootBullet(node: bullet4, action: shoot5)
+            shootBullet(node: bullet5, action: shoot6)
+            shootBullet(node: bullet6, action: shoot7)
+            shootBullet(node: bullet7, action: shoot8)
+            shootBullet(node: bullet8, action: shoot9)
+            shootBullet(node: bullet9, action: shoot10)
         }
         
         rightarrow.didTap = {
@@ -159,5 +165,11 @@ class GameScene: SKScene {
         
     }
     
+    func shootBullet(node: SKSpriteNode, action: SKAction) {
+        addChild(node)
+        node.run(action) {
+            node.removeFromParent()
+        }
+    }
     
 }
